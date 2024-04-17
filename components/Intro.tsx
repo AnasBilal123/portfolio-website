d460c3405b147ab95ac4136e28a470a2359a204e
AnasBilal10 <anasbilal1122786@gmail.com>
2024-04-18 03:00:17 +0500

    Changes in Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -60,10 +60,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Anas Bilal.</span> I'm a{" "}
+        <span className="font-bold">Hello, This is Anas Bilal.</span> I'm a{" "}
         <span className="font-bold">Software Engineer</span> with{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
-        building <span className="italic">website & apps</span>. I am a{" "}
+        building <span className="italic">website & apps</span>. I am
+        experienced
         <span className="underline"> MERN/MEAN Developer</span>.
       </motion.h1>
 
