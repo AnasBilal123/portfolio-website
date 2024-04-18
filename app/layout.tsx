2a773478025b079adbeec002ab6475691ab429ef
AnasBilal10 <anasbilal1122786@gmail.com>
2024-04-18 23:11:21 +0500

    Changes Colors for dark themem

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,12 +29,12 @@ export default function RootLayout({
       </head>
 
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 dark:bg-[#23111e] dark:text-gray-50 relative pt-28 sm:pt-10`}
+        className={`${inter.className} bg-gray-50 text-gray-950 dark:bg-[#030637] dark:text-gray-50 relative pt-28 sm:pt-10`}
       >
         <ActiveSectionContextProvider>
           <ThemeSwitchContext>
-            <div className="dark:bg-[#49243E] bg-[#c3f3f7] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-            <div className="bg-[#f4e0ed] dark:bg-[#773446] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] "></div>
+            <div className="dark:bg-[#3C0753] bg-[#c3f3f7] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+            <div className="bg-[#f4e0ed] dark:bg-[#4f1d42] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] "></div>
             <Header />
             {children}
             <Footer />
